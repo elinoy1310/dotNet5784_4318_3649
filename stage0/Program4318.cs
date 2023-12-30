acbaed24afe988fddb2b3c442bca05c1172e39ca
אלינוי דמרי <elinoy.damari@gmail.com>
2023-12-31 01:20:17 +0200

    Initial Code + Welcome functions

---
+++ stage0/Program4318.cs
@@ -0,0 +1,18 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome4318();
+        Welcome3649();
+        Console.ReadKey();
+    }
+    static partial void Welcome3649();
+    private static void Welcome4318()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine() ?? " ";
+        Console.WriteLine("{0},welcome to my first console application", name);
+    }
+
+   
+}
