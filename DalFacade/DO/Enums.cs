89c0d0fd3a81f0c62af85ef3ff258817d4521aab
אלינוי דמרי <elinoy.damari@gmail.com>
2024-01-07 15:31:45 +0200

    create Task class

--- DalFacade/DO/Enums.cs
+++ DalFacade/DO/Enums.cs
@@ -1,12 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DO;
 
-namespace DO
-{
-    internal class Enums
-    {
-    }
-}
---
+++ DalFacade/DO/Task.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace DO;
+
+public record Task
+(
+    int Id,
+    string Alias,
+    string Description,
+    DateTime CreatedInDate,
+    TimeSpan RequiredEffortTime,
+    bool IsMileStone,
+    DateTime StartDate,
+    DateTime 
+    
+)
+{
+
+}
