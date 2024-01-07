a72d6dfb7dfc76bc957ddce7ef1316b5f410fc62
אלינוי דמרי <elinoy.damari@gmail.com>
2024-01-07 19:00:17 +0200

    fix DataSource class

--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -5,7 +5,7 @@ internal static class DataSource
 {
     internal static List<DO.Engineer?> Engineers { get; } = new();
     internal static List<DO.Dependency?> Dependencys { get; } = new();
-    {
+    
     internal static List<DO.Task?> Tasks { get; } = new();
 }
 
