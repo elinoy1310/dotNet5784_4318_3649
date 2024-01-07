5923e77736aaffe9819fdd74190e386faf09b585
hadarNagar <hadarn26@gmail.com>
2024-01-08 00:35:09 +0200

    Implementation of CRUD functions of IEngineer and IDependency

--- DalList/DependencyImplementation.cs
+++ DalList/DependencyImplementation.cs
@@ -1,12 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace Dal;
 
-namespace Dal
+using System.Collections.Generic;
+using DalApi;
+using DO;
+
+public class DependencyImplementation : IDependency
 {
-    internal class DependencyImplementation
+    /// <summary>
+    /// The function automatically creates a new Id and adds the item with the updated Id to the list
+    /// </summary>
+    /// <param name="item">The item we want to add to the list</param>
+    /// <returns></returns>
+    public int Create(Dependency item) 
     {
+        int newId = DataSource.Config.NextDependencyId;
+        Dependency copy=item with { Id= newId };
+        DataSource.Dependencys.Add(copy);   
+        return newId;   
+    }
+    /// <summary>
+    /// Deleting an existing object with a certain ID from the list
+    /// </summary>
+    /// <param name="id">The ID of the object we want to delete</param>
+    /// <exception cref="Exception">Throwing an exception if the ID we were looking for is not found</exception>
+    public void Delete(int id)
+    {
+        Dependency? found = Read(id);
+        if (found != null)
+        {
+            DataSource.Dependencys.Remove(found);
+            return;
+        }
+        throw new Exception($"Dependency with ID = {id} does not exist");
+    }
+    /// <summary>
+    /// Return if the sent ID is in the list
+    /// </summary>
+    /// <param name="id">The ID of the object we want to find</param>
+    /// <returns></returns>
+    public Dependency? Read(int id)
+    {
+        Dependency? found = DataSource.Dependencys.Find(dep => dep?.Id == id);
+        if (found != null)
+            return found;
+        else
+            return null;    
+    }
+    /// <summary>
+    /// Returning a copy of the list of references to all objects of type Dependency
+    /// </summary>
+    /// <returns></returns>
+    public List<Dependency?> ReadAll()
+    {
+        return new List<Dependency?>(DataSource.Dependencys);
+    }
+    /// <summary>
+    /// Update of an existing object
+    /// </summary>
+    /// <param name="item">The object with the updated details</param>
+    public void Update(Dependency item)
+    {
+        Delete(item.Id);    
+        DataSource.Dependencys.Add(item);   
     }
 }
--- DalList/EngrineerImplementation.cs
+++ DalList/EngrineerImplementation.cs
@@ -1,12 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace Dal;
 
-namespace Dal
+using System.Collections.Generic;
+using DalApi;
+using DO;
+
+
+
+public class EngrineerImplementation : IEngineer
 {
-    internal class EngrineerImplementation
+    /// <summary>
+    /// Checks that the item is not found and adds it to the list
+    /// </summary>
+    /// <param name="item">The item we want to add to the list</param>
+    /// <returns></returns>
+    /// <exception cref="Exception">An exception is thrown if the item is in the list</exception>
+    public int Create(Engineer item)
     {
+        if (Read(item.Id) is not null)
+        throw new Exception($"Engineer with ID={item.Id} already exists");
+        DataSource.Engineers.Add(item); 
+        return item.Id; 
+    }
+    /// <summary>
+    /// Deleting an existing object with a certain ID from the list
+    /// </summary>
+    /// <param name="id">The ID of the object we want to delete</param>
+    /// <exception cref="Exception">Throwing an exception if the ID we were looking for is not found</exception>
+    public void Delete(int id)
+    {
+        Engineer? found=Read(id);   
+        if (found != null) 
+        { 
+            DataSource.Engineers.Remove(found);
+            return;
+        }
+        throw new Exception($"Engineer with ID = {id} was not found");
+    }
+    /// <summary>
+    /// Return if the sent ID is in the list
+    /// </summary>
+    /// <param name="id">The ID of the object we want to find</param>
+    /// <returns></returns>
+    public Engineer? Read(int id)
+    {
+        Engineer? found = DataSource.Engineers.Find(eng => eng?.Id == id);
+        if (found != null)
+            return found;
+        else
+            return null;
+    }
+    /// <summary>
+    /// Returning a copy of the list of references to all objects of type Dependency
+    /// </summary>
+    /// <returns></returns>
+    public List<Engineer?> ReadAll()
+    {
+        return new List<Engineer?>(DataSource.Engineers);
+    }
+    /// <summary>
+    /// Update of an existing object
+    /// </summary>
+    /// <param name="item">The object with the updated details</param>
+    public void Update(Engineer item)
+    {
+        Delete(item.Id);
+        Create(item);
     }
 }
