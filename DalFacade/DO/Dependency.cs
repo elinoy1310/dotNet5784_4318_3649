42f7a5673e40852f29604378686fcfcd9e1f9a61
hadarNagar <hadarn26@gmail.com>
2024-01-07 18:14:58 +0200

    add constructors to Dependency and Engineer and add documentation

--- DalFacade/DO/Dependency.cs
+++ DalFacade/DO/Dependency.cs
@@ -1,10 +1,20 @@
 ﻿
 
 namespace DO;
-
+/// <summary>
+/// Dependency Entity
+/// </summary>
+/// <param name="Id">Personal unique ID of the Dependency</param>
+/// <param name="Dependent">Id number of pending task</param>
+/// <param name="DependsOnTask">Previous assignment Id number</param>
 public record Dependency
 (
     int Id,
-    int Dependent,
-    int DependsOnTask
-);
+    int? Dependent=null,
+    int? DependsOnTask=null
+)
+{
+    //a parameterizes constructor is automatically present
+    public Dependency() : this(0) { } //empty constructor
+
+}
--- DalFacade/DO/Engineer.cs
+++ DalFacade/DO/Engineer.cs
@@ -2,11 +2,23 @@
 
 namespace DO;
 
+/// <summary>
+/// Engineer Entity represents a Engineer with all its props
+/// </summary>
+/// <param name="Id">Personal unique ID of the Engineer</param>
+/// <param name="Email">Private Email of the Engineer</param>
+/// <param name="Cost">How much the Engineer get paid per hour</param>
+/// <param name="Name">Private Name of the Engineer</param>
+/// <param name="Level">Engineer level</param>
 public record Engineer
 (
     int Id,
-    EngineerExperience Level,
     string? Email=null,
     double? Cost=null,
-    string? Name=null
-);
+    string? Name=null,
+    EngineerExperience Level= EngineerExperience.Beginner
+)
+{
+    //a parameterizes constructor is automatically present
+    public Engineer() : this(0) { } //empty constructor
+}
