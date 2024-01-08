f9bc9a936106a7fa4c684d6cac000e15c9e101e7
hadarNagar <hadarn26@gmail.com>
2024-01-08 21:25:59 +0200

    create Functions- NewDependency, ReadSubManu,  ReadAllSubManu, UpdateSubManu, DeleteSubManu

--- DalFacade/DO/Enums.cs
+++ DalFacade/DO/Enums.cs
@@ -1,7 +1,6 @@
 ﻿namespace DO;
 
-public enum EngineerExperience
-{
-    Beginner, AdvancedBeginner, Intermediate, Advanced, Expert
-}
+public enum EngineerExperience{ Beginner, AdvancedBeginner, Intermediate, Advanced, Expert}
+public enum MainManu { Exit, Engineer, Dependency, Task }
+public enum SubManu { exit, create, read, readAll, update, delete }
 
--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,10 +1,10 @@
 ﻿using Dal;
 using DalApi;
+using DO;
 
 namespace DalTest
 {
-    enum MainManue { Exit, Engineer, Dependency, Task};
-    enum SubManue { exit, create, read, readAll, update, delete };
+
     internal class Program
     {
         private static ITask? s_dalTask=new TaskImplementation();
@@ -26,20 +26,20 @@ namespace DalTest
             
         }
 
-        public void PresentMainManue()
+        public void PresentMainManu()
         {
             Console.WriteLine("Select an entity you want to check\r\n0= Exit the main menu\r\n1= engineer\r\n2=dependency\r\n3=task");
-            string chooseMainManue = Console.ReadLine()!;
-            MainManue optionMainManue = (MainManue)int.Parse(chooseMainManue);
-            switch (optionMainManue)
+            string chooseMainManu = Console.ReadLine()!;
+            MainManu optionMainManu = (MainManu)int.Parse(chooseMainManu);
+            switch (optionMainManu)
             {
-                case MainManue.Exit:
+                case MainManu.Exit:
                     break;
-                case MainManue.Engineer:
+                case MainManu.Engineer:
                     break;
-                case MainManue.Dependency:
+                case MainManu.Dependency:
                     break;
-                case MainManue.Task:
+                case MainManu.Task:
                     break;
                 default:
                     break;
@@ -48,23 +48,23 @@ namespace DalTest
 
         
 
-        public void PresentSubMenu(MainManue entity)
+        public void PresentSubMenu(MainManu entity)
         {
-            string chooseSubManue = Console.ReadLine()!;
-            SubManue optionSubMain = (SubManue)int.Parse(chooseSubManue);
+            string chooseSubManu = Console.ReadLine()!;
+            SubManu optionSubMain = (SubManu)int.Parse(chooseSubManu);
             switch (optionSubMain)
             {
-                case SubManue.exit:
+                case SubManu.exit:
                     break;
-                case SubManue.create:
+                case SubManu.create:
                     break;
-                case SubManue.read:
+                case SubManu.read:
                     break;
-                case SubManue.readAll:
+                case SubManu.readAll:
                     break;
-                case SubManue.update:
+                case SubManu.update:
                     break;
-                case SubManue.delete:
+                case SubManu.delete:
                     break;
                 default:
                     break;
@@ -72,6 +72,160 @@ namespace DalTest
 
 
         }
+
+        public Dependency NewDependency()
+        {
+            Console.WriteLine("Enter Id, Dependent, DependsOnTask");
+            int depDependent = int.Parse(Console.ReadLine()?? "0");
+            int depDependsOnTask=int.Parse(Console.ReadLine()?? "0");   
+            Dependency dep = new Dependency(0, depDependent, depDependsOnTask);
+            return dep; 
+        }
+
+        /// <summary>
+        /// Reads and displays information about a specific entity (Engineer, Dependency, or Task) based on user input.
+        /// </summary>
+        /// <param name="entity">The MainManu entity for which information will be read.</param>
+        public void ReadSubManu(MainManu entity)
+        {
+            switch (entity)
+            {
+                case MainManu.Exit:
+                    // No action needed if the main menu option is Exit
+                    return;
+                    //break;
+                case MainManu.Engineer:
+                    // Read Engineer ID from user input
+                    int engId =int.Parse(Console.ReadLine()!);
+                    if (s_dalEndineer!.Read(engId) is not null)// Retrieve and display information about the Engineer with the given ID
+                        Console.WriteLine(s_dalEndineer!.Read(engId));
+                    else
+                        throw new Exception($"Engineer with ID = {engId} was not found");
+                    break;
+                case MainManu.Dependency:
+                    // Read Dependency ID from user input
+                    int depId = int.Parse(Console.ReadLine()!);
+                    if (s_dalDependency!.Read(depId) is not null)// Retrieve and display information about the Dependency with the given ID
+                        Console.WriteLine(s_dalDependency!.Read(depId));
+                    else
+                        throw new Exception($"Dependency with ID = {depId} was not found");
+                    break;
+                case MainManu.Task:
+                    // Read Task ID from user input
+                    int taskId = int.Parse(Console.ReadLine()!);
+                    if (s_dalTask!.Read(taskId) is not null)// Retrieve and display information about the Task with the given ID
+                        Console.WriteLine(s_dalTask!.Read(taskId));
+                    else
+                        throw new Exception($"Task with ID = {taskId} was not found");
+                    break;
+                default:
+                    // No action needed for other MainManu options
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Reads and displays all records for a specific entity (Engineer, Dependency, or Task) based on user input.
+        /// </summary>
+        /// <param name="entity">The MainManu entity for which all records will be read and displayed.</param>
+        public void ReadAllSubManu(MainManu entity)
+        {
+            switch (entity)
+            {
+                case MainManu.Exit:
+                    // No action needed if the main menu option is Exit
+                    return;
+                //break;
+                case MainManu.Engineer:
+                    // Iterate through all Engineers and display each record
+                    foreach (var _engineer in s_dalEndineer!.ReadAll())
+                        Console.WriteLine(_engineer);
+                    break;
+                case MainManu.Dependency:
+                    // Iterate through all Dependencies and display each record
+                    foreach (var _dependency in s_dalDependency!.ReadAll())
+                        Console.WriteLine(_dependency);
+                    break;
+                case MainManu.Task:
+                    // Iterate through all Tasks and display each record
+                    foreach (var _task in s_dalTask!.ReadAll())
+                        Console.WriteLine(_task);
+                    break;
+                default:
+                    // No action needed for other MainManu options
+                    break;
+            }
+        }
+
+        public void UpdateSubManu(MainManu entity)
+        {
+            switch (entity)
+            {
+                case MainManu.Exit:
+                    return;
+                //break;
+                case MainManu.Engineer:
+                    ReadSubManu(entity);
+                    
+                    break;
+                case MainManu.Dependency:
+                    ReadSubManu(entity);
+
+                    break;
+                case MainManu.Task:
+                    ReadSubManu(entity);
+
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Deletes a specific record (Engineer, Dependency, or Task) based on user input.
+        /// </summary>
+        /// <param name="entity">The MainManu entity for which a record will be deleted.</param>
+        public void DeleteSubManu(MainManu entity)
+        {
+            switch (entity)
+            {
+                case MainManu.Exit:
+                    // No action needed if the main menu option is Exit
+                    return;
+                //break;
+                case MainManu.Engineer:
+                    // Read Engineer ID from user input
+                    int engId = int.Parse(Console.ReadLine()!);
+                    // Check if the Engineer with the given ID exists, and delete it if found
+                    if (s_dalEndineer!.Read(engId) is not null)
+                        s_dalEndineer.Delete(engId);
+                    else
+                        throw new Exception($"Engineer with ID = {engId} was not found");
+                    break;
+                case MainManu.Dependency:
+                    // Read Dependency ID from user input
+                    int depId = int.Parse(Console.ReadLine()!);
+                    // Check if the Dependency with the given ID exists, and delete it if found
+                    if (s_dalDependency!.Read(depId) is not null)
+                        s_dalDependency.Delete(depId); 
+                    else
+                        throw new Exception($"Dependency with ID = {depId} was not found");
+                    break;
+                case MainManu.Task:
+                    // Read Task ID from user input
+                    int taskId = int.Parse(Console.ReadLine()!);
+                    // Check if the Task with the given ID exists, and delete it if found
+                    if (s_dalTask!.Read(taskId) is not null)
+                        s_dalTask.Delete(taskId );
+                    else
+                        throw new Exception($"Task with ID = {taskId} was not found");
+                    break;
+                default:
+                    // No action needed for other MainManu options
+                    break;
+            }
+        }
+
     }
 }
 
