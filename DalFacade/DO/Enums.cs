389204e71d57f1b3f5f98c9b64bbb94ce97fe056
אלינוי דמרי <elinoy.damari@gmail.com>
2024-01-08 21:37:46 +0200

    put enums un the right file

--- DalFacade/DO/Enums.cs
+++ DalFacade/DO/Enums.cs
@@ -1,6 +1,8 @@
 ﻿namespace DO;
 
 public enum EngineerExperience{ Beginner, AdvancedBeginner, Intermediate, Advanced, Expert}
-public enum MainManu { Exit, Engineer, Dependency, Task }
-public enum SubManu { exit, create, read, readAll, update, delete }
+public enum MainMenu { Exit, Engineer, Dependency, Task }
+public enum SubMenu { exit, create, read, readAll, update, delete }
 
+//enum MainManue { Exit, Engineer, Dependency, Task };
+//enum SubManue { exit, create, read, readAll, update, delete };
