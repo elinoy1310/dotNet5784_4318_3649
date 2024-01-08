9f65c02e151cb2d40de6a647b1407a82f43b301f
hadarNagar <hadarn26@gmail.com>
2024-01-08 18:25:37 +0200

    the start presentMainManue function for manue

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -3,6 +3,7 @@ using DalApi;
 
 namespace DalTest
 {
+    enum MainManue { Exit, Engineer, Dependency, Task};
     internal class Program
     {
         private static ITask? s_dalTask=new TaskImplementation();
@@ -22,6 +23,26 @@ namespace DalTest
                 Console.WriteLine(ex.Message);
             }
             
+        }
+
+        public void PresentMainManue()
+        {
+            string choose = Console.ReadLine()!;
+            MainManue option = (MainManue)int.Parse(choose);
+            switch (option)
+            {
+                case MainManue.Exit:
+                    break;
+                case MainManue.Engineer:
+                    break;
+                case MainManue.Dependency:
+                    break;
+                case MainManue.Task:
+                    break;
+                default:
+                    break;
+            }
+
         }
     }
 }
