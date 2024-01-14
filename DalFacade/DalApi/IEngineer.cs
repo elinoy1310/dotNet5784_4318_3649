fb54969cbb921254e09a727b62126a3ebf177cc5
hadarNagar <hadarn26@gmail.com>
2024-01-14 15:48:44 +0200

    delete the statements of ITask and IDependency and IEngineer

--- DalFacade/DalApi/IEngineer.cs
+++ DalFacade/DalApi/IEngineer.cs
@@ -2,11 +2,5 @@
 namespace DalApi;
 using DO;
 
-public interface IEngineer
-{
-    int Create(Engineer item); //Creates new entity object in DAL
-    Engineer? Read(int id); //Reads entity object by its ID 
-    List<Engineer> ReadAll(); //stage 1 only, Reads all entity objects
-    void Update(Engineer item); //Updates entity object
-    void Delete(int id); //Deletes an object by its Id
-}
+public interface IEngineer: ICrud<Engineer> { }
+
