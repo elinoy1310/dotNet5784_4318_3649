3accb76a6d175faf673f98272f390907016430a2
hadarNagar <hadarn26@gmail.com>
2024-01-14 16:22:13 +0200

    add Class DalList + Implementation

--- DalList/DalList.cs
+++ DalList/DalList.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Dal;
+using DalApi;
 
-namespace Dal
+sealed public class DalList : IDal
 {
-    internal class DalList
-    {
-    }
+    public IDependency Dependency => new DependencyImplementation();
+
+    public IEngineer Engineer => new EngineerImplementation();
+
+    public ITask Task => new TaskImplementation();
 }
