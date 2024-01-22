a6975b0f8f351ea15006c6600ee0356da0d88b00
hadarNagar <hadarn26@gmail.com>
2024-01-22 21:56:36 +0200

    save DalXml class

--- DalXml/DalXml.cs
+++ DalXml/DalXml.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using DalApi;
+namespace Dal;
 
-namespace Dal
+public class DalXml : IDal
 {
-    internal class DalXml
-    {
-    }
+    public IDependency Dependency => new DependencyImplementation();
+
+    public IEngineer Engineer =>  new EngineerImplementation();
+
+    public ITask Task =>  new TaskImplementation();
 }
