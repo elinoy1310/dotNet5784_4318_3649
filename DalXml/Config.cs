2e4202f8e4eeb92efe17c7ce266196a563696126
אלינוי דמרי <elinoy.damari@gmail.com>
2024-01-22 18:58:29 +0200

    add Config class in DalXml file+ initialize the run number of Dependency and Task

---
+++ DalXml/Config.cs
@@ -0,0 +1,9 @@
+﻿namespace Dal;
+
+internal static class Config
+{
+    static string s_data_config_xml = "data-config";
+    internal static int NextDependencyId { get => XMLTools.GetAndIncreaseNextId(s_data_config_xml, "NextDependencyId"); }
+    internal static int NextTaskId { get => XMLTools.GetAndIncreaseNextId(s_data_config_xml, "NextTaskId"); }
+
+}
