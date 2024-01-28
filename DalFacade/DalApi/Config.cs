deb60273be4027f9a8944cd961ae20fa51470b5c
hadarNagar <hadarn26@gmail.com>
2024-01-28 14:38:32 +0200

    save class Config

--- DalFacade/DalApi/Config.cs
+++ DalFacade/DalApi/Config.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DalApi;
+using System.Xml.Linq;
 
-namespace DalApi
+static class Config
 {
-    internal class Config
+    /// <summary>
+    /// internal PDS class
+    /// </summary>
+    internal record DalImplementation
+    (string Package,   // package/dll name
+     string Namespace, // namespace where DAL implementation class is contained in
+     string Class   // DAL implementation class name
+    );
+
+    internal static string s_dalName;
+    internal static Dictionary<string, DalImplementation> s_dalPackages;
+
+    static Config()
     {
+        XElement dalConfig = XElement.Load(@"..\xml\dal-config.xml") ??
+  throw new DalConfigException("dal-config.xml file is not found");
+
+        s_dalName =
+           dalConfig.Element("dal")?.Value ?? throw new DalConfigException("<dal> element is missing");
+
+        var packages = dalConfig.Element("dal-packages")?.Elements() ??
+  throw new DalConfigException("<dal-packages> element is missing");
+        s_dalPackages = (from item in packages
+                         let pkg = item.Value
+                         let ns = item.Attribute("namespace")?.Value ?? "Dal"
+                         let cls = item.Attribute("class")?.Value ?? pkg
+                         select (item.Name, new DalImplementation(pkg, ns, cls))
+                        ).ToDictionary(p => "" + p.Name, p => p.Item2);
     }
 }
+
+[Serializable]
+public class DalConfigException : Exception
+{
+    public DalConfigException(string msg) : base(msg) { }
+    public DalConfigException(string msg, Exception ex) : base(msg, ex) { }
+}
+
