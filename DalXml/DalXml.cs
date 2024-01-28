a62642f15e1364294e244eafc17001e954d36a3e
hadarNagar <hadarn26@gmail.com>
2024-01-28 15:36:20 +0200

    fix the DalXml class

--- DalXml/DalXml.cs
+++ DalXml/DalXml.cs
@@ -4,7 +4,7 @@ using DalApi;
 
 sealed internal class DalXml : IDal
 {
-    public static DalXml Instance = new DalXml();
+    public static IDal Instance { get; } = new DalXml();
 
     public IDependency Dependency => new DependencyImplementation();
 
