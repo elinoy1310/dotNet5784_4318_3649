67a9ca6eed70f273a82c71174bccd0c897d216cc
אלינוי דמרי <elinoy.damari@gmail.com>
2024-01-28 18:06:24 +0200

    add classes: Engineer,EngineerInTask,MileStone,MilestoneInList,TaskInEngineer

---
+++ BL/BO/EngineerInTask.cs
@@ -0,0 +1,7 @@
+﻿namespace BO;
+
+public class EngineerInTask
+{
+    public int Id { get; init; }
+    public string? Name { get; set; }
+}
---
+++ BL/BO/MilestoneInList.cs
@@ -0,0 +1,7 @@
+﻿namespace BO;
+
+public class MilestoneInList
+{
+    public int Id { get; init; }
+    public string? Alias { get; set; }
+}
