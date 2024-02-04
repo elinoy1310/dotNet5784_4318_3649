97c8d6790c27b9e6e6acfa95b5d597f887c92131
hadarNagar <hadarn26@gmail.com>
2024-02-04 15:18:25 +0200

    create interface - ITask

--- BL/BlApi/ITask.cs
+++ BL/BlApi/ITask.cs
@@ -1,6 +1,11 @@
 ﻿namespace BlApi;
 public interface ITask
 {
-
+    public IEnumerable<BO.Task> ReadAll(Func<Task, bool>? filter = null);
+    public BO.Task Read(int id);
+    public void Add(BO.Task task);
+    public void Update(BO.Task task);
+    public void Delete(int id);
+    public void CreateStartDate(int id,DateTime date);
 }
 
