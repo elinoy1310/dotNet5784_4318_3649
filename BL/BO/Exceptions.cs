30a3079f4c20173f0f138f349dfa75dd0db88631
אלינוי דמרי <elinoy.damari@gmail.com>
2024-02-05 21:12:57 +0200

    update Exceptions

--- BL/BO/Exceptions.cs
+++ BL/BO/Exceptions.cs
@@ -6,24 +6,36 @@ namespace BO;
 public class BlDoesNotExistException : Exception
 {
     public BlDoesNotExistException(string? message) : base(message) { }
+    public BlDoesNotExistException(string message, Exception innerException)
+                : base(message, innerException) { }
+
 }
 
 [Serializable]
 public class BlAlreadyExistException : Exception
 {
     public BlAlreadyExistException(string? message) : base(message) { }
+    public BlAlreadyExistException(string message, Exception innerException)
+                : base(message, innerException) { }
+
 }
 
 [Serializable]
 public class BlWrongInputFormatException : Exception
 {
     public BlWrongInputFormatException(string? message) : base(message) { }
+    public BlWrongInputFormatException(string message, Exception innerException)
+                : base(message, innerException) { }
+
 }
 
 [Serializable]
 public class BlCanNotBeNullException : Exception
 {
     public BlCanNotBeNullException(string? message) : base(message) { }
+    public BlCanNotBeNullException(string message, Exception innerException)
+                : base(message, innerException) { }
+
 }
 
 [Serializable]
