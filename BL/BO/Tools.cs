8c5bdaa6e8d1b11156c8b9a0a1166ad2b965fd23
אלינוי דמרי <elinoy.damari@gmail.com>
2024-02-05 23:42:10 +0200

    update ToStringProperty

--- BL/BO/Tools.cs
+++ BL/BO/Tools.cs
@@ -11,19 +11,19 @@ public static class Tools
         foreach (PropertyInfo item in t!.GetType().GetProperties())
         {
             str += "\n" + item.Name + ": " + item.GetValue(t, null);
-            if (item.GetType() == typeof(IEnumerable<T>))
-                str += item.ToStringProperty();
-            //if (item.PropertyType.IsGenericType && item.PropertyType.GetGenericTypeDefinition() == typeof(IEnumerable<>))
-            //{
-            //    // אם סוג התכונה הוא אוסף, הדפסת האיברים
-            //    IEnumerable<T> collection = (IEnumerable<T>)item.GetValue(t);
-            //    str += string.Join(", ", collection);
-            //}
-            //else
-            //{
-            //    // אם לא, הדפסת הערך
-            //    str += item.GetValue(t, null);
-            //}
+            //if (item.GetType() == typeof(IEnumerable<T>))
+            //    str += item.ToStringProperty();
+            if (item.PropertyType.IsGenericType && item.PropertyType.GetGenericTypeDefinition() == typeof(IEnumerable<>))
+            {
+                // אם סוג התכונה הוא אוסף, הדפסת האיברים
+                IEnumerable<T> collection = (IEnumerable<T>)item.GetValue(t);
+                str += string.Join(", ", collection);
+            }
+            else
+            {
+                // אם לא, הדפסת הערך
+                str += item.GetValue(t, null);
+            }
 
         }
         return str;
