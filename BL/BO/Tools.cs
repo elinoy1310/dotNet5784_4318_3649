2ab91e994fa46eb037f9d7ab464bc2fc2932ba22
אלינוי דמרי <elinoy.damari@gmail.com>
2024-02-05 22:09:41 +0200

    fix ToStringProperty

--- BL/BO/Tools.cs
+++ BL/BO/Tools.cs
@@ -8,11 +8,11 @@ public static class Tools
     public static string ToStringProperty<T>(this T t)
     {
         string str = "";
-        foreach (PropertyInfo item in t.GetType().GetProperties())
-        { 
-            if (item.GetType() == typeof(IEnumerable<T>))
-                str += item.ToStringProperty();
+        foreach (PropertyInfo item in t!.GetType().GetProperties())
+        {
             str += "\n" + item.Name + ": " + item.GetValue(t, null);
+            if (item.GetType() == typeof(IEnumerable<T>))
+                str += item.ToStringProperty();           
         }
         return str;
     }
