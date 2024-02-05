dfaaaa8518514dd1c5b8d85c587b8c64bc49ad65
hadarNagar <hadarn26@gmail.com>
2024-02-05 20:42:39 +0200

    save Bl

--- BL/BlImplementation/Bl.cs
+++ BL/BlImplementation/Bl.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace BlImplementation
+using BlApi;
+
+namespace BlImplementation;
+
+internal class Bl : IBl
 {
-    internal class Bl
-    {
-    }
+    public IEngineer Engineer => new EngineerImplementation();
+
+    public ITask Task => new TaskImplementation();
 }
