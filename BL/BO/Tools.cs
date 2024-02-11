8ea44ef6930bd5e757cd2945e9e09f17f054ccfe
hadarNagar <hadarn26@gmail.com>
2024-02-11 20:51:15 +0200

    change the function ToStringProperty

--- BL/BO/Tools.cs
+++ BL/BO/Tools.cs
@@ -8,67 +8,65 @@ namespace BO;
 
 public static class Tools
 {
-    //public static string ToStringProperty<T>(this T t, string suffix = "")
-    //{
-    //    string str = "";
-    //    foreach (PropertyInfo prop in t.GetType().GetProperties())
-    //    {
-    //        var value = prop.GetValue(t, null);
-    //        if (value is IEnumerable)
-    //            foreach (var item in (IEnumerable)value)
-    //                str += item.ToStringProperty("   ");
-    //        else
-    //            str += "\n" + suffix + prop.Name + ": " + value;
-    //    }
-    //    return str;
-    //}
-
-    public static string ToStringProperty<T>(this T t) where T : notnull
-    {
-        var str = new StringBuilder();
-        var properties = t!.GetType().GetProperties();
-        var newLine = Environment.NewLine;
-
-        foreach (var property in properties)
+        public static string ToStringProperty<T>(this T t, string suffix = "")
         {
-            var value = property.GetValue(t);
-            if (value is IEnumerable<T>)
+            string str = "";
+            foreach (PropertyInfo prop in t.GetType().GetProperties())
             {
-                string strValue = $"{property.Name}:{newLine} {string.Join(newLine, value)}";
-                str.AppendLine(strValue);
+                var value = prop.GetValue(t, null);
+                if (value is IEnumerable)
+                    foreach (var item in (IEnumerable)value)
+                        str += item.ToStringProperty("   ");
+                else
+                    str += "\n" + suffix + prop.Name + ": " + value;
             }
-            str.AppendLine($"{property.Name}: {value}");
+            return str;
         }
-        return str.ToString();
-    }
-        //string str = "";
-        //foreach (PropertyInfo item in t!.GetType().GetProperties())
-        //{
-        //    str += "\n" + item.Name + ": " + item.GetValue(t, null);
+}
+    //public static string ToStringProperty<T>(this T t) where T:notnull
+    //{
+    //    var str = new StringBuilder();
+    //    var properties = t!.GetType().GetProperties();
+    //    var newLine = Environment.NewLine;
 
-        //    if (item.GetType() == typeof(System.Collections.Generic.List`1[BO.TaskInList]))
-        //    {
-        //        str += item.ToStringProperty();
-        //    }
-        //    //    //        //foreach (PropertyInfo item2 in item!.GetType().GetProperties())
-        //    //        //{
-        //    //        //        str += "\n" + item2.Name + ": ";
-        //    //        //    foreach (PropertyInfo item3 in item2.GetType().GetProperties())
-        //    //        //        str += item2.GetValue(t, null);
-        //    //        //}
-        //    //            //    str += item.ToStringProperty();
-        //    //            //if (item.PropertyType.IsGenericType && item.PropertyType.GetGenericTypeDefinition() == typeof(IEnumerable<>))
-        //    //            //{
-        //    //            //    // אם סוג התכונה הוא אוסף, הדפסת האיברים
-        //    //            //    IEnumerable<T> collection = (IEnumerable<T>)item.GetValue(t);
-        //    //            //    str += string.Join(", ", collection);
-        //    //            //}
-        //    //            //else
-        //    //            //{
-        //    //            //    // אם לא, הדפסת הערך
-        //    //            //    str += item.GetValue(t, null);
-        //    //            //}
+    //    foreach (var property in properties)
+    //    {
+    //        var value = property.GetValue(t);
+    //        if (value is IEnumerable<T>)
+    //        {
+    //            string strValue = $"{property.Name}:{newLine} {string.Join(newLine, value)}";
+    //            str.AppendLine(strValue);
+    //        }
+    //        str.AppendLine($"{property.Name}: {value}");
+    //    }
+    //    return str.ToString();
+    //string str = "";
+    //foreach (PropertyInfo item in t!.GetType().GetProperties())
+    //{
+    //    str += "\n" + item.Name + ": " + item.GetValue(t, null);
 
-        //    }
-        // return str;
-    }
+    //    if (item.GetType() == typeof(System.Collections.Generic.List`1[BO.TaskInList]))
+    //    {
+    //        str += item.ToStringProperty();
+    //    }
+    //    //    //        //foreach (PropertyInfo item2 in item!.GetType().GetProperties())
+    //    //        //{
+    //    //        //        str += "\n" + item2.Name + ": ";
+    //    //        //    foreach (PropertyInfo item3 in item2.GetType().GetProperties())
+    //    //        //        str += item2.GetValue(t, null);
+    //    //        //}
+    //    //            //    str += item.ToStringProperty();
+    //    //            //if (item.PropertyType.IsGenericType && item.PropertyType.GetGenericTypeDefinition() == typeof(IEnumerable<>))
+    //    //            //{
+    //    //            //    // אם סוג התכונה הוא אוסף, הדפסת האיברים
+    //    //            //    IEnumerable<T> collection = (IEnumerable<T>)item.GetValue(t);
+    //    //            //    str += string.Join(", ", collection);
+    //    //            //}
+    //    //            //else
+    //    //            //{
+    //    //            //    // אם לא, הדפסת הערך
+    //    //            //    str += item.GetValue(t, null);
+    //    //            //}
+
+    //    }
+    // return str;
