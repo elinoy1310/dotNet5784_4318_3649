78daebad164054d8b9d208f42b90851c2f0b6599
אלינוי דמרי <elinoy.damari@gmail.com>
2024-02-19 23:33:55 +0200

    add dp add_updateEngineer+ connecting the data by id in the constructor to the controls text box, combo box

--- PL/Engineer/EngineerWindow.xaml.cs
+++ PL/Engineer/EngineerWindow.xaml.cs
@@ -1,27 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace PL.Engineer
 {
+
     /// <summary>
     /// Interaction logic for EngineerWindow.xaml
     /// </summary>
     public partial class EngineerWindow : Window
     {
-        public EngineerWindow()
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+        public BO.EngineerExperience Level { get; set; } = BO.EngineerExperience.None;
+        public EngineerWindow(int idEngineer = 0)
         {
             InitializeComponent();
+            if (idEngineer == 0)
+            {
+                add_updateEngineer = new BO.Engineer();
+            }
+            else
+            {
+                try
+                {
+                    add_updateEngineer = s_bl.Engineer.Read(idEngineer);
+                }
+                catch (BO.BlDoesNotExistException ex)
+                {
+                    MessageBoxResult mbResult = MessageBox.Show(ex.Message,"Error",MessageBoxButton.OK,MessageBoxImage.Error);
+                    if (mbResult == MessageBoxResult.OK) 
+                    {
+                        new EngineerListWindow().Show();
+                    }
+                }
+
+            }
         }
+
+
+        public BO.Engineer add_updateEngineer
+        {
+            get { return (BO.Engineer)GetValue(add_updateEngineerProperty); }
+            set { SetValue(add_updateEngineerProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for add_updateEngineer.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty add_updateEngineerProperty =
+            DependencyProperty.Register("add_updateEngineer", typeof(BO.Engineer), typeof(EngineerWindow), new PropertyMetadata(new BO.Engineer()));
+
+
     }
 }
