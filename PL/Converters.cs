720823f34d5f39c383583d747dbe3df2c25aff4f
אלינוי דמרי <elinoy.damari@gmail.com>
2024-02-27 01:01:09 +0200

    design EnginerListWindow

--- PL/Converters.cs
+++ PL/Converters.cs
@@ -1,6 +1,8 @@
 ﻿
 using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Windows;
 using System.Windows.Data;
 using BO;
@@ -55,6 +57,26 @@ public class ConvertTextToIsEnabled : IValueConverter
         //{
         //    return Visibility.Visible;
         //}
+    }
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class ConvertListToVisibility : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        IEnumerable<BO.Engineer> lstEng = (IEnumerable<BO.Engineer>)value;
+        if (lstEng.Count()==0)
+        {
+            return Visibility.Visible;
+        }
+        else
+        {
+            return Visibility.Hidden;
+        }
     }
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
